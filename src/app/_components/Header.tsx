63f702bb742d22f6021a2a39fa4a59a7180eb755
hiromu <mi326hi@gmail.com>
2025-08-17 01:33:10 +0900

    PascalCase　再修正

---
+++ src/app/_components/Header.tsx
@@ -0,0 +1,42 @@
+"use client";
+import Link from "next/link";
+import { useSupabaseSession } from "../_hooks/useSupabaseSession";
+import { supabase } from "@/utils/supabase";
+import React from "react";
+
+
+export const Header: React.FC = () => {
+  const handleLogout = async () => {
+    await supabase.auth.signOut();
+    window.location.href = "/";
+  };
+  const { session, isLoading } = useSupabaseSession();
+  return (
+    <header className="flex justify-between p-6 bg-[#333] text-[#fff] font-bold">
+      <Link href="/" className="header-link">
+        Blog
+      </Link>
+      {!isLoading && (
+        <div className="flex items-center gap-4">
+          {session ? (
+            <>
+              <Link href="/admin" className="header-link">
+                管理画面
+              </Link>
+              <button onClick={handleLogout}>ログアウト</button>
+            </>
+          ) : (
+            <>
+              <Link href="/contact" className="header-link">
+                お問い合わせ
+              </Link>
+              <Link href="/login" className="header-link">
+                ログイン
+              </Link>
+            </>
+          )}
+        </div>
+      )}
+    </header>
+  );
+};
---
+++ src/app/_types/Post.ts
@@ -0,0 +1,10 @@
+import { Category } from "@/app/_types/Category";
+
+export type Post = {
+  id: number;
+  title: string;
+  content: string;
+  createdAt: string;
+  postCategories: { category: Category }[];
+  thumbnailImageKey: string;
+};
